cb6b2214af4b898f56e0735f020509c48e5a4cd9
Alexander Shevchenko <aleksandr.shevchenko@gmail.com>
2026-01-29 00:29:00 +0200

    read and write some Java file (content)

--- src/main/java/com/zmpc/app/App.java
+++ src/main/java/com/zmpc/app/App.java
@@ -1,9 +1,19 @@
 package com.zmpc.app;
 
+import com.zmpc.util.FileUtils;
+
+import java.io.IOException;
+
 import static com.zmpc.common.Print.println;
+import static com.zmpc.util.FileUtils.readTextFromFile;
 
 public class App {
-    public static void main(String[] args) {
-        println("Hello World!");
+    public static void main(String[] args) throws IOException {
+        String originFilePath = "D:\\Workspace\\Java\\2025\\Solutions\\JavaCodeSmartTracer\\tracer\\origin\\App.java";
+        String outFilePath = "D:\\Workspace\\Java\\2025\\Solutions\\JavaCodeSmartTracer\\tracer\\out\\App.java";
+
+        String content = readTextFromFile(originFilePath);
+        println(content);
+        FileUtils.writeTextToFile(content, outFilePath);
     }
 }
--- src/main/java/com/zmpc/util/FileUtils.java
+++ src/main/java/com/zmpc/util/FileUtils.java
@@ -1,15 +1,16 @@
 package com.zmpc.util;
 
+import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 
 public class FileUtils {
 
-    public static String readTextFromFile(String filePath) throws Exception {
+    public static String readTextFromFile(String filePath) throws IOException {
         return Files.readString(Path.of(filePath));
     }
 
-    public static void writeTextToFile(String text, String filePath) throws Exception {
+    public static void writeTextToFile(String text, String filePath) throws IOException {
         Files.writeString(Path.of(filePath), text);
     }
 }
